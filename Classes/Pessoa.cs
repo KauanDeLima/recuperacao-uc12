5bae60cdde4ae78e591aa96688e2f739742a5a3d
Kauan <Kauancrdl3@gmail.com>
2022-10-27 22:27:40 -0300

    primeiro commit da recuperação

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,18 @@
+
+
+
+
+
+namespace Projeto_Recuperacao.Classes
+
+
+{
+         public abstract class Pessoa
+
+         
+    {
+        public string? Nome { get; set; }
+        public float Rendimento { get; set; }   
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿// menu de boas vindas
+
+
+
+using Projeto_Recuperacao.Classes;
+Console.WriteLine(@"
+
+             Bem vindo ao sistema   
+                de cadastro          
+
+");
+string? opcao;
+
+do
+{
+    Console.WriteLine(@"
+    
+    |    Escolha uma das opões   |     
+
+    |   1 - Cadastrar PJ         |
+    |   2 - Listar PJ            |
+    |   0 - Sair                 |
+
+    ");
+    opcao = Console.ReadLine();
+
+    PessoaJuridica metodosPj = new PessoaJuridica();
+
+
+
+
+
+    switch (opcao)
+    {
+        case "1":
+            PessoaJuridica pj = new PessoaJuridica();
+
+            Console.WriteLine("Digite o nome da PJ");
+            pj.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o rendimento");
+            pj.Rendimento = float.Parse(Console.ReadLine());
+
+            bool valido;
+            do
+            {
+                Console.WriteLine("Informe o Cnpj ex: XX.XXX.XXX/0001-XX ou XXXXXXXX0001XX");
+                pj.Cnpj = Console.ReadLine();
+
+                
+                valido = pj.ValidarCnpj(pj.Cnpj);
+
+            } while (valido == false);
+
+            metodosPj.Inserir(pj);
+
+
+            break;
+        case "2":
+
+            List<PessoaJuridica> listaPj = metodosPj.Ler();
+
+
+            foreach (PessoaJuridica cadaItem in listaPj)
+
+            {
+                Console.WriteLine(@$"
+                    Nome: {cadaItem.Nome}
+
+
+                    Rendimento: {cadaItem.Rendimento}
+
+                    Cnpj: {cadaItem.Cnpj}
+                    ");
+                Console.WriteLine("Aperte Enter para continuar");
+
+                Console.ReadLine();
+            }
+
+
+
+            break;
+              case "0":
+             break;
+             default:
+            Console.WriteLine("Opção inválida, escolha outra opção");
+            break;
+    }
+} while (opcao != "0");
